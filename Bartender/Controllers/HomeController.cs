6cf7f3395df11dbb735480b9e4508c24a11c32f0
Christopher Carter <chris.mi.carter@gmail.com>
2020-08-29 19:39:59 -0400

    Added order queue page

--- Bartender/Controllers/HomeController.cs
+++ Bartender/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,6 +11,7 @@ namespace Bartender.Controllers
     public class HomeController : Controller
     {
         private CocktailContext db = new CocktailContext();
+        private OrderContext orderDB = new OrderContext();
 
         public ActionResult Index()
         {
@@ -21,23 +23,70 @@ namespace Bartender.Controllers
             return View(db.Cocktails.ToList());
         }
 
-        public ActionResult Create()
-        {
-            return View();
-        }
 
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(Cocktail cocktail)
+        public ActionResult Menu(string customer_name, string drink)
         {
-            if (ModelState.IsValid)
+            if (customer_name != "" && drink != null)
             {
-                db.Cocktails.Add(cocktail);
-                db.SaveChanges();
-                return RedirectToAction("Index");
+                orderDB.Orders.Add(new Order
+                {
+                    OrderNo = orderDB.Orders.OrderByDescending(o => o.OrderNo).First().OrderNo + 1,
+                    Cocktail = drink,
+                    Customer = customer_name,
+                    Status = Order.State.PREPARING
+                });
+                orderDB.SaveChanges();
             }
 
-            return View(cocktail);
+            return View("Index");
+        }
+
+        public ActionResult Create(string name, string customer)
+        {
+            if(name != null && customer != null)
+            {
+                orderDB.Orders.Add(new Order
+                {
+                    OrderNo = orderDB.Orders.OrderByDescending(o => o.OrderNo).First().OrderNo + 1,
+                    Cocktail = name,
+                    Customer = customer,
+                    Status = Order.State.PREPARING
+                });
+            }
+
+            return View("Index");
+        }
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Create(Cocktail cocktail)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Cocktails.Add(cocktail);
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
+
+        //    return View(cocktail);
+        //}
+
+        public ActionResult Orders()
+        {
+            return View(orderDB.Orders.ToList());
+        }
+
+        public ActionResult Ready(int? id)
+        {
+            if(id != null)
+            {
+                var result = orderDB.Orders.Single(o => o.OrderNo == id);
+                result.Status = Order.State.READY;
+                orderDB.SaveChanges();
+            }
+
+            return View("Orders", orderDB.Orders.ToList());
         }
     }
 }
---
+++ Bartender/Models/Order.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Bartender.Models
+{
+    public class Order
+    {
+        public enum State { PREPARING, READY }
+
+        [Key]
+        public int OrderNo { get; set; }
+        public string Cocktail { get; set; }
+        public string Customer { get; set; }
+        public State Status { get; set; }
+
+    }
+
+    public class OrderContext : DbContext
+    {
+        public OrderContext() : base("BartenderDB")
+        {
+
+        }
+
+        public DbSet<Order> Orders { get; set; }
+    }
+}
